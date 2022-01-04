22ae51adc03bdcb3b63077c697b4eced3f488c47
rashidotz <rashidotz@gmail.com>
2022-01-04 15:32:15 +0300

    04-1-22

--- models/Conversation.js
+++ models/Conversation.js
@@ -1,5 +1,4 @@
 import { Schema, model } from "mongoose";
-
 const ConversationSchema = new Schema(
     {
         members: {
@@ -17,4 +16,24 @@ try {
     Conversation = model('Conversation', ConversationSchema)
 }
 module.exports = Conversation
+// =======================================================
+// import mongoose from 'mongoose';
+// var Schema = mongoose.Schema;
 
+// const ConversationSchema = new Schema(
+//     {
+//         members: {
+//             type: Array,
+//             required: true
+//         }
+//     },
+
+//     { timestamps: true }
+// );
+
+
+// mongoose.models = {};
+
+// var Conversation = mongoose.model('Conversation', ConversationSchema);
+
+// export default Conversation;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import connect from "../../utils/dbConnect"
 export default function helloAPI(req, res) {
+    connect();
   res.status(200).json({ name: 'John Doe' })
 }
