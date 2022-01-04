b9dbc3237fe661216009c88011c2390295e181cd
muhammedrashidm <rashidotz@gmail.com>
2022-01-04 12:44:01 +0300

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connect from "../../utils/dbConnect"
 
-export default function helloAPI(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default function helloAPI(req, res) {  
+const con =  connect();
+  
+res.status(200).json({ res: con })
 }
