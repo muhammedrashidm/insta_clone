c53af4a6947033f396c44103fb494ec29b0dbd95
muhammedrashidm <rashidotz@gmail.com>
2022-01-14 23:43:29 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAAW5vnhjb3IHuJOrq3embNMkQxFsQtVGE",
-    authDomain: "rasd-instagram.firebaseapp.com",
-    projectId: "rasd-instagram",
-    storageBucket: "rasd-instagram.appspot.com",
-    messagingSenderId: "658178103570",
-    appId: "1:658178103570:web:85455569e19f604974fbcb"
-};
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+}
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
