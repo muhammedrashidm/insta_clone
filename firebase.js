d52c1a5ef654758717dd701b384af6a24d0f943d
muhammedrashidm <rashidotz@gmail.com>
2022-01-15 14:43:03 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,7 +9,7 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  projectId: 'rasd-instagram',
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
